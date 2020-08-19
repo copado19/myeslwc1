7452427324110e76c661e22590909ce4337a721d
Anurag Verma <anuragtrimurti19@gmail.com>
2020-08-19 01:22:43 -0400

    WIP

---
+++ es-space-mgmt/main/default/lwc/customerList/customerList.js
@@ -0,0 +1,25 @@
+import { LightningElement, api, wire  } from 'lwc';
+import getCustomerList from '@salesforce/apex/reservationManagementController.getCustomerList';
+export default class CustomerList extends LightningElement {
+    
+    @api sobject = 'Lead';
+    customers = [];
+
+    errorMsg;
+    msgForUser;
+    wiredRecords;
+
+    @wire(getCustomerList, { sObjectType: '$sobject' })
+    wiredCustomerData(value) {
+        console.log(JSON.stringify(value));
+        this.wiredRecords = value;
+        if (value.error) {
+            this.errorMsg = value.error;
+            this.msgForUser = 'There was an issue loading customers.';
+        } else if (value.data) {
+            this.customers = value.data;
+        }
+    }
+    
+}
+
